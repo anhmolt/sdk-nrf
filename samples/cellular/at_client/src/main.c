a03655ea129389c8a0d7f39a88075e8b23773c44
Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>
2024-08-05 16:20:38 +0200

    samples: cellular: at_client sample nrf92
    Support at_client sample on nrf9280.
    
    Signed-off-by: Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>

--- samples/cellular/at_client/src/main.c
+++ samples/cellular/at_client/src/main.c
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <modem/nrf_modem_lib.h>
+#include <nrf_modem_at.h>
+
+#if defined(CONFIG_SOC_SERIES_NRF91X)
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
@@ -22,6 +25,9 @@ void enable_xtal(void)
 	sys_notify_init_spinwait(&cli.notify);
 	(void)onoff_request(clk_mgr, &cli);
 }
+#endif /* CONFIG_SOC_SERIES_NRF91X */
+
+static char buf[128] = {};
 
 int main(void)
 {
@@ -34,8 +40,20 @@ int main(void)
 		printk("Modem library initialization failed, error: %d\n", err);
 		return 0;
 	}
+#if defined(CONFIG_SOC_SERIES_NRF91X)
 	enable_xtal();
+#endif /* CONFIG_SOC_SERIES_NRF91X */
 	printk("Ready\n");
+
+#if defined(CONFIG_SOC_SERIES_NRF92X)
+	/* Print IMEI */
+	err = nrf_modem_at_cmd(buf, sizeof(buf), "AT+CGSN=1");
+	if (err) {
+		printk("Failed AT+CGSN=1, err %d\n", err);
+	} else {
+		printk("Success: buf: %.127s\n", buf);
+	}
+#endif /* CONFIG_SOC_SERIES_NRF92X */
 
 	return 0;
 }
