04da4e2c94e403eef4d33f3a0bcdf222e04658d4
Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>
2024-10-25 09:59:49 +0200

    temp

--- samples/cellular/at_client/src/main.c
+++ samples/cellular/at_client/src/main.c
@@ -10,6 +10,10 @@
 #include <modem/nrf_modem_lib.h>
 #include <nrf_modem_at.h>
 
+#if defined(CONFIG_DK_LIBRARY)
+#include <dk_buttons_and_leds.h>
+#endif /* CONFIG_DK_LIBRARY */
+
 #if defined(CONFIG_SOC_SERIES_NRF91X)
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/clock_control.h>
@@ -27,6 +31,16 @@ void enable_xtal(void)
 }
 #endif /* CONFIG_SOC_SERIES_NRF91X */
 
+#if defined(CONFIG_DK_LIBRARY)
+void buttons_handler(uint32_t button_state, uint32_t has_changed)
+{
+	printk("button_handler: button 1: %u, button 2: %u "
+	       "button 3: %u, button 4: %u\n",
+	       (bool)(button_state & DK_BTN1_MSK), (bool)(button_state & DK_BTN2_MSK),
+	       (bool)(button_state & DK_BTN3_MSK), (bool)(button_state & DK_BTN4_MSK));
+}
+#endif /* CONFIG_DK_LIBRARY */
+
 static char buf[128] = {};
 
 int main(void)
@@ -34,6 +48,61 @@ int main(void)
 	int err;
 
 	printk("The AT host sample started\n");
+
+
+#if defined(CONFIG_DK_LIBRARY)
+	err = dk_leds_init();
+	if (err) {
+		printk("Failed led init, err %d\n", err);
+	}
+
+	err = dk_set_leds_state(0x0, DK_ALL_LEDS_MSK);
+	if (err) {
+		printk("Failed setting leds state, err: %d\n", err);
+	}
+
+	k_msleep(1000);
+
+	err = dk_set_leds_state(0x1, DK_ALL_LEDS_MSK);
+	if (err) {
+		printk("Failed setting leds state, err: %d\n", err);
+	}
+
+	k_msleep(1000);
+
+	err = dk_set_leds_state(0x2, DK_ALL_LEDS_MSK);
+	if (err) {
+		printk("Failed setting leds state, err: %d\n", err);
+	}
+
+	k_msleep(1000);
+
+	err = dk_set_leds_state(0x4, DK_ALL_LEDS_MSK);
+	if (err) {
+		printk("Failed setting leds state, err: %d\n", err);
+	}
+
+	k_msleep(1000);
+
+	err = dk_set_leds_state(0x8, DK_ALL_LEDS_MSK);
+	if (err) {
+		printk("Failed setting leds state, err: %d\n", err);
+	}
+
+	k_msleep(1000);
+
+	err = dk_set_leds_state(0xF, DK_ALL_LEDS_MSK);
+	if (err) {
+		printk("Failed setting leds state, err: %d\n", err);
+	}
+
+	k_msleep(1000);
+
+	err = dk_buttons_init(buttons_handler);
+	if (err) {
+		printk("Failed buttons init, err %d\n", err);
+	}
+#endif /* CONFIG_DK_LIBRARY */
 
 	err = nrf_modem_lib_init();
 	if (err) {
